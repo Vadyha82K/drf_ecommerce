3a4a847a791e6538b826c311191559f310b7ff2e
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-02-27 12:10:30 +0300

    Создал базовый класс модели (BaseModel), включвающий общие поля и методы для всех можелей

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,25 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from apps.common.managers import GetOrNoneManager
+
+
+class BaseModel(models.Model):
+    """
+        Базовый класс модели, который включает в себя общие поля и методы для всех моделей.
+
+        Attributes:
+            id (UUIDField): Уникальный идентификатор экземпляра модели.
+            created_at (DateTimeField): Отметка времени, когда экземпляр был создан.
+            updated_at (DateTimeField): Отметка времени последнего обновления экземпляра.
+    """
+
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, unique=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = GetOrNoneManager()
+
+    class Meta:
+        abstract = True
