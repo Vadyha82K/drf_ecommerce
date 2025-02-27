1adb13f133885fb3e7add7c8ffe13e7411ed2fc7
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-02-27 21:14:45 +0300

    Создал класс ShippingAddress, который представляет собой адрес доставки, связанный с пользователем

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from apps.accounts.models import User
+from apps.common.models import BaseModel
+
+
+class ShippingAddress(BaseModel):
+    """
+        Представляет собой адрес доставки, связанный с пользователем.
+
+        Атрибуты:
+            user (ForeignKey): Пользователь, которому принадлежит адрес доставки;
+            full_name (str): Полное имя получателя;
+            email (str): Адрес электронной почты получателя;
+            phone (str): Номер телефона получателя;
+            address (str): Уличный адрес получателя;
+            city (str): Город получателя;
+            country (str): Страна получателя;
+            zipcode (in): Почтовый индекс получателя.
+
+        Методы:
+            __str__():
+                Возвращает строковое представление сведений о доставке.
+        """
+
+    user = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = "shipping_addresses"
+    )
+    full_name = models.CharField(max_length=1000)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, null=True)
+    address = models.CharField(max_length=1000, null=True)
+    city = models.CharField(max_length=200, null=True)
+    country = models.CharField(max_length=200, null=True)
+    zipcode = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.full_name}'s shipping details"
