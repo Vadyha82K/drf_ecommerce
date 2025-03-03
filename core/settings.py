11ad6a1e547203b0c4f046ab0a48089b2f88d572
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-03-03 15:41:38 +0300

    Подключил в settings.py использование JWTAuthentication из библиотеки djangorestframework-simplejwt в качестве класса аутентификации по умолчанию для всех эндпоинтов API.

--- core/settings.py
+++ core/settings.py
@@ -139,6 +139,9 @@ AUTH_USER_MODEL = "accounts.User"
 
 # DRF
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
