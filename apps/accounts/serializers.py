94d55b42d8af0c29a025a1462a1a4daef7899a83
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-03-04 20:42:57 +0300

    Добавил класс MyTokenObtainPairSerializer, который служит для добавления данных в payload токена, который создается с помощью TokenObtainPairView

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from apps.accounts.models import User
 
@@ -9,5 +10,27 @@ class CreateUserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('email', 'password')
 
-        def validate_password(self, value: str):
-            return make_password(value)
+    def validate_password(self, value: str) -> str:
+        return make_password(value)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Класс служит для добавления данных в payload токена, который создается с помощью TokenObtainPairView
+    """
+    @classmethod
+    def get_token(cls, user):
+        """
+        Метод сериализатора, который добавляет информацию о группе и роли пользователя в полезную нагрузку токенов
+        (refresh и access)
+        """
+        token = super().get_token(user)
+
+        # Добавляем пользовательские данные в полезную нагрузку
+        if user.is_staff:
+            token['group'] = 'admin'
+        else:
+            token['group'] = 'user'
+            token['role'] = user.account_type
+
+        return token
