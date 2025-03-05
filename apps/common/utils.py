94c6ba42b3d710e9ffbe52406a8141686866af2a
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-03-05 12:35:45 +0300

    Создал функцию set_dict_attr в common/utils.py, которая позволяет обновлять атрибуты объекта динамически

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -21,4 +21,17 @@ def generate_unique_code(model: BaseModel, field: str) -> str:
     similar_object_exist = model.objects.filter(**{field: code}).exists()
     if not similar_object_exist:
         return code
+
     return generate_unique_code(model, field)
+
+
+def set_dict_attr(obj, data):
+    """
+    Эта функция позволяет обновлять атрибуты объекта динамически, используя данные из словаря. Это особенно полезно,
+    если количество атрибутов, которые нужно обновить, неизвестно заранее или меняется. Вместо множественного
+    присваивания user.attr1 = ..., user.attr2 = ... и т.д., используется один вызов set_dict_attr
+    """
+    for attr, value in data.items():
+        setattr(obj, attr, value)
+
+        return obj
