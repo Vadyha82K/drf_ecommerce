ba32e3c93250c8b8f5f0ab506999f3dc1e7cd613
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-03-06 15:48:46 +0300

    Добавил три сериализатора: SellerShopSerializer, ProductSerializer, CreateProductSerializer

--- apps/shop/serializers.py
+++ apps/shop/serializers.py
@@ -5,3 +5,45 @@ class CategorySerializer(serializers.Serializer):
     name = serializers.CharField()
     slug = serializers.SlugField(read_only=True)
     image = serializers.ImageField()
+
+
+class SellerShopSerializer(serializers.Serializer):
+    """
+    Этот сериализатор используется для сериализации данных о продавце (магазине).
+    """
+    name = serializers.CharField(source="business_name")
+    slug = serializers.CharField()
+    avatar = serializers.CharField(source="user.avatar")
+
+
+class ProductSerializer(serializers.Serializer):
+    """
+    Этот сериализатор предназначен для сериализации данных о продукте. Он использует вложенные сериализаторы для
+    продавца и категории.
+    """
+    seller = SellerShopSerializer()
+    name = serializers.CharField()
+    slug = serializers.SlugField()
+    desc = serializers.CharField()
+    price_old = serializers.DecimalField(max_digits=10, decimal_places=2)
+    price_current = serializers.DecimalField(max_digits=10, decimal_places=2)
+    category = CategorySerializer()
+    in_stock = serializers.IntegerField()
+    image1 = serializers.ImageField()
+    image2 = serializers.ImageField(required=False)
+    image3 = serializers.ImageField(required=False)
+
+
+class CreateProductSerializer(serializers.Serializer):
+    """
+    Этот сериализатор, предназначен для создания продукта. Он не использует вложенных сериализаторов для продавца и
+    категории, а использует category_slug — slug категории передается напрямую.
+    """
+    name = serializers.CharField(max_length=100)
+    desc = serializers.CharField()
+    price_current = serializers.DecimalField(max_digits=10, decimal_places=2)
+    category_slug = serializers.CharField()
+    in_stock = serializers.IntegerField()
+    image1 = serializers.ImageField()
+    image2 = serializers.ImageField(required=False)
+    image3 = serializers.ImageField(required=False)
