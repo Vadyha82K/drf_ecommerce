bbdb7c6df0e198d3f870636541d0f812257e7b53
Vadim <120470243+Vadyha82K@users.noreply.github.com>
2025-03-06 16:17:32 +0300

    Добавил маршрут к представлению для работы спродуктами ProductsBySellerView

--- apps/sellers/urls.py
+++ apps/sellers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.sellers.views import SellersView
+from apps.sellers.views import SellersView, ProductsBySellerView
 
 urlpatterns = [
     path("", SellersView.as_view()),
+    path("products/", ProductsBySellerView.as_view()),
 ]
